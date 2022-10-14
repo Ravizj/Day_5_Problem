958c9e92eb29eee23525965f636eaad4e6e2c9d8
Ravizj <ravizj57@gmail.com>
2022-10-14 16:01:57 +0530

    This is my Day Five Problem

---
+++ src/Leap_Year.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Leap_Year {
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter four Digit n for the year=");
+        int x=sc.nextInt();
+
+        if(x/400==0)
+        {
+            System.out.println(x+" is a leap year");
+        }
+        else {
+            System.out.println(x+" is  a Leap Year or not.");
+        }
+    }
+
+}
